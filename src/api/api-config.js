b9b208a836c30f1d2c4f5ca548a4433ea1991b91
Sergey Krivulko <echo.042014@gmail.com>
2023-12-17 20:49:46 +0300

    final

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -1,13 +1,14 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyA6a0235mLzfgVSXaB3QSNzrh8dBoaacuY",
-    authDomain: "itacademy-fed9f.firebaseapp.com",
-    databaseURL: "https://itacademy-fed9f-default-rtdb.firebaseio.com",
-    projectId: "itacademy-fed9f",
-    storageBucket: "itacademy-fed9f.appspot.com",
-    messagingSenderId: "588909858724",
-    appId: "1:588909858724:web:1c5deda1a30e8859a0bb31"
-}
+  apiKey: "AIzaSyDgQGLstRkUSaGPwkKecOV1GwjvivYtGmk",
+  authDomain: "testproject-a897f.firebaseapp.com",
+  databaseURL: "https://testproject-a897f-default-rtdb.firebaseio.com",
+  projectId: "testproject-a897f",
+  storageBucket: "testproject-a897f.appspot.com",
+  messagingSenderId: "1071968991206",
+  appId: "1:1071968991206:web:b86ea167ad19abc631c827",
+  measurementId: "G-TB65W204XK",
+};
 
 Object.freeze(FIREBASE_CONFIG);
 
-export const DB_URL = 'https://itacademy-fed9f-default-rtdb.firebaseio.com/';
+export const DB_URL = FIREBASE_CONFIG.databaseURL;
